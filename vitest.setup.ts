f95b0893d38110ab1279c1da8d2e7839bbc15fde
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-06-28 03:35:01 +0200

    Add Vitest for test units

---
+++ vitest.setup.ts
@@ -0,0 +1,9 @@
+import { beforeEach, afterEach } from "vitest";
+
+beforeEach(() => {
+  // Código que se ejecuta antes de cada prueba
+});
+
+afterEach(() => {
+  // Código que se ejecuta después de cada prueba
+});
